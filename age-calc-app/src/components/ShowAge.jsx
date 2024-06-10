1744b8c5fae2cd9e9cafd7f399cdf1d5033410f3
Eric Kibuchi <119484375+CodedVeli@users.noreply.github.com>
2024-06-10 17:23:19 +0300

    Update ShowAge.jsx

--- age-calc-app/src/components/ShowAge.jsx
+++ age-calc-app/src/components/ShowAge.jsx
@@ -8,24 +8,24 @@ const ShowAge = () => {
 
     const isNumber = (num) => !isNaN(num)
     return (
-        <section className='flex flex-col mt-1 text-start gap-1 text-6xl text-Purple'>
-           <p>
-            <span>{isNumber(calculatedYears) ? calculatedYears: "--"}
+        <section className='flex flex-col mt-1 text-start gap-1 text-6xl '>
+           <p  className="text-black">
+            <span className="text-Purple">{isNumber(calculatedYears) ? calculatedYears: "--"}
                 </span>
                 {" "}
                 years
             
            </p>
-           <p>
-            <span >
+           <p  className="text-black">
+            <span className="text-Purple">
                 {isNumber(calculatedMonths) ? calculatedMonths:"--"} 
             </span>
             {" "}
                 months
             
            </p>
-           <p>
-            <span>
+           <p className="text-black">
+            <span className="text-Purple">
                 {isNumber(calculatedDays) ? calculatedDays:"--"}
             </span>
             {" "}
